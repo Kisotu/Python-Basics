ea9aea66ae132aa31a6c066742c91fe97a8df9a6
Oloishorua <jacoboloishorua@gmail.com>
2024-01-06 23:02:08 +0300

    explicit type conversions in python

---
+++ type_conversions.py
@@ -0,0 +1,20 @@
+import math
+
+
+# To int
+balance = 45.75
+area = 34.234
+
+print("Balance to int is: ", int(balance))
+print("Area to int: ", int(area))
+
+# To float
+
+radius = 7
+print("Area of circle is : ", math.pi * float(radius) * float(radius))
+
+# To string
+age = 87
+name = "Kile"
+
+print(f"{name} is {str(age)} years old")
