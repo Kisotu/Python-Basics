fb57e6ee35c6f015238895538ee6cdaea9f0648c
Oloishorua <jacoboloishorua@gmail.com>
2024-01-06 22:44:43 +0300

    working with imports and modules in python

---
+++ imports&modules.py
@@ -0,0 +1,13 @@
+# built in imports
+import math as m
+
+# user defined imports
+import addition
+
+summ = addition.add(45, 6)
+
+print(f"The sum is {summ}")
+radius = 7.0
+
+area = m.pi * radius * radius
+print(f"Area of the circle is: {area}")
