aea00aa6a453c00a45073690472a668569edd2a3
Oloishorua <jacoboloishorua@gmail.com>
2024-01-06 22:51:47 +0300

    printing a list using for in

---
+++ days.py
@@ -0,0 +1,10 @@
+# prints days of the week
+days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+for x in days:
+    if x == "Tuesday":
+        continue
+    print(x)
+str1 = "Storage"
+
+print("Length of str1 is ", len(str1))
